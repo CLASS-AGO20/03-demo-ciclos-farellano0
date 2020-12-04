b10619032f7b5c038e06b0efeaa778f9620a60e7
Fernando Arellano <farellano0@ucol.mx>
2020-12-03 18:31:27 -0600

    implemented contarImparesFor()

--- src/main.js
+++ src/main.js
@@ -9,8 +9,21 @@ export default class App {
 
         return suma;
     }
+
+    contarImparesFor(inicio, fin){
+        let impar = 0
+
+        for(let i = inicio; i <= fin; i = i + 1){
+            if(i % 2 != 0){
+                impar = impar + 1;
+            }
+        }
+        return impar;
+    }
+
 }
 
 let app = new App();
 
 console.log(app.sumarParesFor());
+console.log(app.contarImparesFor(5, 11));
